dc7734d01011b0de02d9add2c76681c594f25f0e
alchassefe <alchassefe@a108pc15L.local.isima.fr>
2024-11-04 09:50:30 +0100

    importation librairies

--- src/main/java/AwesomePasswordChecker.java
+++ src/main/java/AwesomePasswordChecker.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import java.util.ArrayList;
 import java.util.List;
 
